75c1a81e080dfa3cd2b4fa517246536e408f7bf2
dyns <dylan.schumacker+github@outlook.fr>
2025-03-27 17:29:15 +0100

    ⚙️ init: project setup step 1

---
+++ src/main/java/com/dyns/persevero/PerseveroApplication.java
@@ -0,0 +1,13 @@
+package com.dyns.persevero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerseveroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerseveroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dyns/persevero/PerseveroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyns.persevero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerseveroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
