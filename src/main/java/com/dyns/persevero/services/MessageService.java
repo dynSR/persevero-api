5cd8e0105f53d49a92b49d80e95c6ff036232838
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-04-18 11:18:29 +0200

    💉 fix: problem with locale and getting correct key's associated message

--- src/main/java/com/dyns/persevero/services/MessageService.java
+++ src/main/java/com/dyns/persevero/services/MessageService.java
@@ -1,23 +1,28 @@
 package com.dyns.persevero.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 @Component
 public class MessageService {
     private final MessageSource messageSource;
 
+    @Autowired
     public MessageService(MessageSource messageSource) {
         this.messageSource = messageSource;
     }
 
-    public String getMessage(String key, Object... args) {
-        return messageSource.getMessage(key, args, Locale.getDefault());
+    public String getMessage(String key) {
+        return getMessage(key, (Object) null);
     }
 
-    public String getMessage(String key) {
-        return messageSource.getMessage(key, null, Locale.getDefault());
+    public String getMessage(String key, Object... args) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(key, args, locale);
     }
 }
