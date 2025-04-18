a73f0dbb2a663eb4bf5b34fc2dfab6da7dd16648
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-04-18 11:23:23 +0200

    🩺 refactor: removed findAllByMuscleId from repo

--- src/main/java/com/dyns/persevero/repositories/ExerciseRepository.java
+++ src/main/java/com/dyns/persevero/repositories/ExerciseRepository.java
@@ -1,7 +1,6 @@
 package com.dyns.persevero.repositories;
 
 import com.dyns.persevero.domain.model.impl.Exercise;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +12,4 @@ public interface ExerciseRepository extends CrudRepository<Exercise, UUID> {
 
     Optional<Exercise> findByName(String name);
 
-    @Query("SELECT e FROM Exercise e JOIN e.muscles m WHERE m.id = :muscleId")
-    Iterable<Exercise> findAllByMuscleId(UUID muscleId);
 }
