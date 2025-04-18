3372aed6c9cb04aee55065dcd02f869727d8b437
Dylan Schumacker <Dylan.schumacker@outlook.fr>
2025-04-18 11:27:35 +0200

    💉 fix: changed dto pt.2

--- src/main/java/com/dyns/persevero/domain/dto/WorkoutLogDto.java
+++ src/main/java/com/dyns/persevero/domain/dto/WorkoutLogDto.java
@@ -1,21 +1,36 @@
 package com.dyns.persevero.domain.dto;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.validator.constraints.Range;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
-@Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class WorkoutLogDto {
-    // TODO: Set validation messages
 
-    @NotNull(message = "")
-    @Min(value = 1, message = "{value}")
+    private UUID id;
+
+    @Min(value = 1L)
     private float totalDuration;
+
+    private LocalDateTime completedAt;
+
+    private UserDto owner;
+
+    private void setId(UUID uuid) {
+        id = uuid;
+    }
+
+    private void completedAt(LocalDateTime dateTime) {
+        completedAt = dateTime;
+    }
 }
